89d5f7754ddee7b3863552d5eaa960f2fd0b0a25
Vartikaa-max <vartikarastogi0948@gmail.com>
2026-05-18 01:27:44 +0530

    Add files via upload

--- script.jsx
+++ script.jsx
@@ -1,37 +1,44 @@
-function calculateResult(){
-
-let n = document.getElementById("subjects").value;
-
-let total = 0;
-
-for(let i = 1; i <= n; i++){
-
-let marks = parseFloat(prompt("Enter marks for Subject " + i));
-
-total = total + marks;
-
+let employees = [];
+function addEmployee() {
+let name = document.getElementById("name").value;
+let empId = document.getElementById("empId").value;
+let salary = parseFloat(document.getElementById("salary").value);
+let dept = document.getElementById("dept").value;
+if (name === "" || empId === "" || isNaN(salary) || dept === "") {
+alert("Please fill all fields properly");
+return;
 }
-
-let percentage = total / n;
-
-let grade = "";
-
-if(percentage >= 90){
-grade = "A";
+let employee = {
+name: name,
+id: empId,
+salary: salary,
+department: dept
+};
+employees.push(employee);
+alert("Employee Added Successfully!");
+document.getElementById("name").value = "";
+document.getElementById("empId").value = "";
+document.getElementById("salary").value = "";
+document.getElementById("dept").value = "";
 }
-else if(percentage >= 75){
-grade = "B";
+function displayEmployees() {
+let output = "<h3>All Employees</h3>";
+employees.forEach(emp => {
+output += `
+Name: ${emp.name} |
+ID: ${emp.id} |
+Salary: ₹${emp.salary} |
+Dept: ${emp.department} <br>
+`;
+});
+document.getElementById("output").innerHTML = output;
 }
-else if(percentage >= 50){
-grade = "C";
-}
-else{
-grade = "Fail";
-}
-
-document.getElementById("result").innerHTML =
-"Total Marks: " + total +
-"<br>Percentage: " + percentage.toFixed(2) + "%" +
-"<br>Grade: " + grade;
-
-}
+function filterSalary() {
+let filtered = employees.filter(emp => emp.salary > 50000);
+let output = "<h3>Employees with Salary > ₹50,000</h3>";
+filtered.forEach(emp => {
+output += `
+Name: ${emp.name} |
+Salary: ₹${emp.salary} <br>
+`;
+});
