8d5306a3322a7af79bbea5deda2a8b70eb2685b4
Vartikaa-max <vartikarastogi0948@gmail.com>
2026-06-12 21:17:29 +0530

    Added all experiments

---
+++ MT1practical/MT1script.jsx
@@ -0,0 +1,12 @@
+function calculateSum() {
+    let n = document.getElementById("num").value;
+    let sum = 0;
+
+    for (let i = 1; i <= n; i++) {
+        if (i % 2 === 0) {
+            sum += i;
+        }
+    }
+
+    document.getElementById("result").innerHTML = "Sum = " + sum;
+}
